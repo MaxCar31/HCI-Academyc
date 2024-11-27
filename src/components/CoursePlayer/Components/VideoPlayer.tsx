73ec8422f44cf556a8ef4194dffad4ce787f853a
Max Carrión <maxmateo.carrion@epn.edu.ec>
2024-11-26 23:18:06 -0500

    change: video of CoursePlayer

--- src/components/CoursePlayer/Components/VideoPlayer.tsx
+++ src/components/CoursePlayer/Components/VideoPlayer.tsx
@@ -1,4 +1,3 @@
-import { PlayCircle } from 'lucide-react';
 import { Lesson } from '@/data/courseContent';
 
 type VideoPlayerProps = {
@@ -9,20 +8,16 @@ export default function VideoPlayer({ currentLesson }: VideoPlayerProps) {
   return (
     <div className="relative">
       <div className="aspect-video bg-black relative">
-        {currentLesson.imagen ? (
-          <img
-            src={currentLesson.imagen}
-            alt="Miniatura del video"
-            className="object-cover w-full h-auto"
-          />
-        ) : (
-          <div className="flex items-center justify-center h-full">
-            <PlayCircle className="w-16 h-16 text-white opacity-80" />
-          </div>
-        )}
-        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
-          <PlayCircle className="w-16 h-16 text-white opacity-80" />
-        </div>
+        <iframe
+          width="100%"
+          height="100%"
+          src="https://www.youtube.com/embed/6KqNsLirbps"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          className="object-cover w-full h-full"
+        ></iframe>
       </div>
       <h2 className="text-2xl font-bold px-6 py-4">{currentLesson.title}</h2>
     </div>
